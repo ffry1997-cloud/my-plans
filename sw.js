3f0ccd5c2c55eb53b2f27aa5e9241adc8592d249
ffry1997-cloud <ffry1997@gmail.com>
2026-06-05 18:58:49 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,35 @@
-const CACHE_NAME = 'my-plans-v3';
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => e.waitUntil(caches.keys().then(names => Promise.all(names.map(n => caches.delete(n))))));
-self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
+const CACHE_NAME = 'my-plans-v4';
+
+self.addEventListener('install', event => {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(['/', 'index.html']);
+        })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    if (event.request.method !== 'GET') return;
+    event.respondWith(
+        caches.match(event.request).then(cached => {
+            return cached || fetch(event.request).then(response => {
+                if (response.ok) {
+                    const clone = response.clone();
+                    caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+                }
+                return response;
+            }).catch(() => cached);
+        })
+    );
+});
